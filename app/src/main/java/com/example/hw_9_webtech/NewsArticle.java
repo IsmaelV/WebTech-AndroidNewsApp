0dfd2fff01065dcd01eb85c02353f6b014acd6df
IsmaelV <iv2181@columbia.edu>
2020-05-02 20:34:42 -0700

    Added JSON parsing capabilities | Create object of NewsArticle by parsing through JSON and saving the object

---
+++ app/src/main/java/com/example/hw_9_webtech/NewsArticle.java
@@ -0,0 +1,93 @@
+package com.example.hw_9_webtech;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NewsArticle {
+    private String title;
+    private String imgURL;
+    private String section;
+    private String content;
+    private String articleID;
+    private String webURL;
+    private String date;
+
+    public NewsArticle(JSONObject article, String type) {
+        if (type.equals("home")){
+            extractHome(article);
+        }
+    }
+
+    private void extractHome(JSONObject article){
+        try {
+            // Set all values
+            title = article.getString("webTitle");
+            articleID = article.getString("id");
+            webURL = article.getString("webUrl");
+            section = article.getString("sectionId");
+            date = article.getString("webPublicationDate");
+            content = article.getJSONObject("blocks")
+                    .getJSONArray("body")
+                    .getJSONObject(0)
+                    .getString("bodyTextSummary");
+
+            // Search for appropriate image
+            boolean imgFound = false;
+            JSONArray imgPath;
+            try{
+                imgPath = article.getJSONObject("blocks")
+                        .getJSONObject("main").getJSONArray("elements")
+                        .getJSONObject(0).getJSONArray("assets");
+            }
+            catch(Exception e){
+                imgPath = article.getJSONObject("blocks")
+                        .getJSONArray("body").getJSONObject(0)
+                        .getJSONArray("elements").getJSONObject(0)
+                        .getJSONArray("assets");
+            }
+            for (int j = 0; j < imgPath.length(); j++){
+                int w = imgPath.getJSONObject(j).getJSONObject("typeData").getInt("width");
+                if (w >= 2000){
+                    imgURL = imgPath.getJSONObject(j).getString("file");
+                    imgFound = true;
+                    break;
+                }
+            }
+            if (!imgFound){
+                imgURL = "https://assets.guim.co.uk/images/eada8aa27c12fe2d5afa3a89d3fbae0d/fallback-logo.png";
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getImgURL(){
+        return imgURL;
+    }
+
+    public String getSection(){
+        return section;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String getArticleID() {
+        return articleID;
+    }
+
+    public String getWebURL() {
+        return webURL;
+    }
+
+    public String getDate() {
+        return date;
+    }
+}
--- app/src/main/java/com/example/hw_9_webtech/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/home/HomeFragment.java
@@ -11,10 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.hw_9_webtech.NewsArticle;
 import com.example.hw_9_webtech.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -23,21 +33,28 @@ import java.util.Random;
 public class HomeFragment extends Fragment {
 
     private ListView listView;
-    private List<String> dataPoints;
     private SwipeRefreshLayout r;
+    private RequestQueue mqueue;
+    private JSONArray results;
+    private List<NewsArticle> all_news;
+    private List<String> tmp_list;  // Only temporarily used to display text in a list
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        mqueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
+        tmp_list = new ArrayList<>();
+        all_news = new ArrayList<>();
+        jsonParse();
+
         r = root.findViewById(R.id.refresher);
-        listView = root.findViewById(R.id.yoink_list);
-        dataPoints = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.yoink)));
+        listView = root.findViewById(R.id.home_list);
         listView.setAdapter(
                 new ArrayAdapter<>(
                         Objects.requireNonNull(getActivity()),
                         android.R.layout.simple_list_item_1,
-                        dataPoints
+                        tmp_list
                 )
         );
 
@@ -53,13 +70,46 @@ public class HomeFragment extends Fragment {
     }
 
     private void shuffle(){
-        Collections.shuffle(dataPoints, new Random(System.currentTimeMillis()));
+        Collections.shuffle(tmp_list, new Random(System.currentTimeMillis()));
         listView.setAdapter(
                 new ArrayAdapter<>(
                         Objects.requireNonNull(getActivity()),
                         android.R.layout.simple_list_item_1,
-                        dataPoints
+                        tmp_list
                 )
         );
+    }
+
+    private void jsonParse() {
+        String apiURL = "https://ivillega-nytimes-guardian.wl.r.appspot.com/guardian/home";
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                apiURL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            results = response.getJSONObject("response")
+                                    .getJSONArray("results");
+
+                            for(int i = 0; i < results.length(); i++){
+                                NewsArticle singleArticle = new NewsArticle(results.getJSONObject(i), "home");
+                                tmp_list.add(singleArticle.getTitle());
+                                all_news.add(singleArticle);
+                            }
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        mqueue.add(request);
     }
 }
