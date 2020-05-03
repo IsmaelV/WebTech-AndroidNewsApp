b8c3280f211bcc571b66d1b1d02ad98b1984e965
IsmaelV <iv2181@columbia.edu>
2020-05-02 18:44:45 -0700

    Created VERY simple array into a listView | Refresh capability that simply shuffles the list

--- app/src/main/java/com/example/hw_9_webtech/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/home/HomeFragment.java
@@ -4,32 +4,61 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.hw_9_webtech.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    ListView listView;
+    List<String> dataPoints;
+    SwipeRefreshLayout r;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        r = root.findViewById(R.id.refresher);
+        listView = (ListView) root.findViewById(R.id.yoink_list);
+        dataPoints = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.yoink)));
+        listView.setAdapter(
+                new ArrayAdapter(
+                        getActivity(),
+                        android.R.layout.simple_list_item_1,
+                        dataPoints
+                )
+        );
+
+        r.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onRefresh() {
+                shuffle();
+                r.setRefreshing(false);
             }
         });
+
         return root;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(dataPoints, new Random(System.currentTimeMillis()));
+        listView.setAdapter(new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, dataPoints));
     }
 }
