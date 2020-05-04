545f3ebd52ce52d218245b31f703135a781dc136
IsmaelV <iv2181@columbia.edu>
2020-05-03 20:11:59 -0700

    Dabbling with SharedPreferences as a mock database

--- app/src/main/java/com/example/hw_9_webtech/MainActivity.java
+++ app/src/main/java/com/example/hw_9_webtech/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.hw_9_webtech;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.example.hw_9_webtech.db.BookmarkViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,16 +22,14 @@ import pub.devrel.easypermissions.EasyPermissions;
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_LOCATION_PERMISSION = 1;
-//    private static final int NEW_BOOKMARK_ACTIVITY_REQUEST_CODE = 1;
-//    private String TAG = this.getClass().getSimpleName();
-//    private BookmarkViewModel bookmarkViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestLocationPermission();
-//        bookmarkViewModel = ViewModelProviders.of(this).get(BookmarkViewModel.class);
+        SharedPreferences pref = this.getSharedPreferences("MyBookmarks", 0);
+//        SharedPreferences.Editor editor = pref.edit();
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -42,18 +40,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == NEW_BOOKMARK_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
-//            Toast.makeText(getApplicationContext(), R.string.bookmarked, Toast.LENGTH_SHORT).show();
-//        }
-//        else{
-//            Toast.makeText(getApplicationContext(), R.string.not_bookmarked, Toast.LENGTH_LONG).show();
-//        }
-//    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
