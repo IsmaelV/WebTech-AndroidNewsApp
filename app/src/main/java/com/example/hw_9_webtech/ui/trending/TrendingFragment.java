726d5dd78f09cf445f318d77292502e78f7a1c4b
IsmaelV <iv2181@columbia.edu>
2020-05-04 00:51:19 -0700

    Finished trending tab completely

--- app/src/main/java/com/example/hw_9_webtech/ui/trending/TrendingFragment.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/trending/TrendingFragment.java
@@ -1,6 +1,7 @@
 package com.example.hw_9_webtech.ui.trending;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.hw_9_webtech.R;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -36,30 +38,34 @@ import java.util.Objects;
 public class TrendingFragment extends Fragment {
 
     private List<Entry> entryList;
-//    private ArrayAdapter myArrayAdapter;
     private LineData myLineData;
     private LineChart lineChart;
-    private RequestQueue mqueue;
+    private RequestQueue my_queue;
     private JSONArray all_weeks;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_trending, container, false);
-        mqueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
+        my_queue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
         lineChart = root.findViewById(R.id.trending_chart);
+//        Legend leg = lineChart.getLegend();  // Set Legend Later
+//        leg.setDrawInside(true);
+//        leg.setFormSize(10f);
+//        leg.setForm(Legend.LegendForm.SQUARE);
+//        leg.setXEntrySpace(5f);
+//        leg.setYEntrySpace(5f);
         entryList = new ArrayList<>();
-//        myArrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()), android.R.layout.simple_list_item_1, entryList);
 
         final EditText editText = root.findViewById(R.id.trending_query);
-//        editText.setOnKeyListener(new View.OnKeyListener(){
-//            public boolean onKey(View v, int keyCode, KeyEvent event){
-//                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
-//                    Toast.makeText(getActivity(), editText.getText(), Toast.LENGTH_SHORT).show();
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
+        editText.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event){
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+                    getChartInfo(editText.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
 
         String init_query = editText.getText().toString();
         if(!init_query.equals("")){
@@ -69,16 +75,12 @@ public class TrendingFragment extends Fragment {
             getChartInfo("Coronavirus");
         }
 
-//        lineChart.setData(myLineData);
-//        lineChart.invalidate();  // refresh
         return root;
     }
 
     private LineData retrieveTrendingData(String q){
-        System.out.println("LINE 84 - EntryList: " + entryList);
         LineDataSet lineDataSet = new LineDataSet(entryList, q);
         lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        System.out.println("LineDataSet: " + lineDataSet.toSimpleString());
 
         List<ILineDataSet> finalDataSet = new ArrayList<>();
         finalDataSet.add(lineDataSet);
@@ -86,33 +88,7 @@ public class TrendingFragment extends Fragment {
     }
 
     private void getChartInfo(final String q) {
-//        ServerController controller = new ServerController();
-//        controller.parseDataForVolleyRequest(mqueue, q, new TrendingServerCallback() {
-//            @Override
-//            public void onSuccessTrending(JSONObject result) {
-//                try {
-//                    all_weeks = result.getJSONObject("default")
-//                            .getJSONArray("timelineData");
-//                    for(int i = 0; i < all_weeks.length(); i++){
-//                        entryList.add(
-//                                new Entry(
-//                                        i,
-//                                        all_weeks.getJSONObject(i).getJSONArray("value").getInt(0)
-//                                )
-//                        );
-//                    }
-//                }
-//                catch (JSONException e){
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//            @Override
-//            public void onErrorTrending(VolleyError err) {
-//                err.printStackTrace();
-//            }
-//        });
-
+        entryList.clear();
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
                 "https://ivillega-nytimes-guardian.wl.r.appspot.com/google/interest_over_time/" + q,
@@ -135,7 +111,6 @@ public class TrendingFragment extends Fragment {
                             myLineData = retrieveTrendingData(q);
                             lineChart.setData(myLineData);
                             lineChart.invalidate();  // refresh
-//                            myArrayAdapter.notifyDataSetChanged();
                         }
                         catch (JSONException e){
                             e.printStackTrace();
@@ -148,6 +123,6 @@ public class TrendingFragment extends Fragment {
                         error.printStackTrace();
                     }
                 });
-        mqueue.add(request);
+        my_queue.add(request);
     }
 }
