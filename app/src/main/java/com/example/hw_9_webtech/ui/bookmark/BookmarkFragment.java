7d4fcb98a2e8fbbb53c9b5a9ce11d92342c8f17b
IsmaelV <iv2181@columbia.edu>
2020-05-03 20:10:06 -0700

    Added grid view to the bookmark tab | Does not look great

--- app/src/main/java/com/example/hw_9_webtech/ui/bookmark/BookmarkFragment.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/bookmark/BookmarkFragment.java
@@ -4,32 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hw_9_webtech.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class BookmarkFragment extends Fragment {
 
-    private BookmarkViewModel bookmarkViewModel;
+    private GridView gridView;
+    private List<String> all_bookmarked;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        bookmarkViewModel =
-                ViewModelProviders.of(this).get(BookmarkViewModel.class);
         View root = inflater.inflate(R.layout.fragment_bookmark, container, false);
-        final TextView textView = root.findViewById(R.id.text_bookmark);
-        bookmarkViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        String[] t_list = new String[] {"one", "two", "three", "four", "I am just trying to test one really long text vs some really short text", "six"};
+        all_bookmarked = new ArrayList<>(Arrays.asList(t_list));
+
+        gridView = root.findViewById(R.id.bookmark_list);
+        gridView.setAdapter(
+                new ArrayAdapter<>(
+                        Objects.requireNonNull(getActivity()),
+                        android.R.layout.simple_list_item_1,
+                        all_bookmarked
+                )
+        );
+
         return root;
     }
 }
