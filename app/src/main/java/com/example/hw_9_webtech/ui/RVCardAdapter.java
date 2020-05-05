edf9670fa232bc337358a61a85b0b7a12b948d61
IsmaelV <iv2181@columbia.edu>
2020-05-05 14:24:37 -0700

    Fixed date display for articles

--- app/src/main/java/com/example/hw_9_webtech/ui/RVCardAdapter.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/RVCardAdapter.java
@@ -1,5 +1,8 @@
 package com.example.hw_9_webtech.ui;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.TimeZone;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +14,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.hw_9_webtech.NewsArticle;
 import com.example.hw_9_webtech.R;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
+
+// Great GREAT resource for RecyclerView:
+// https://code.tutsplus.com/tutorials/getting-started-with-recyclerview-and-cardview-on-android--cms-23465
 
 public class RVCardAdapter extends RecyclerView.Adapter<RVCardAdapter.ArticleViewHolder>{
     private List<NewsArticle> myNews;
@@ -48,7 +56,17 @@ public class RVCardAdapter extends RecyclerView.Adapter<RVCardAdapter.ArticleVie
     public void onBindViewHolder(ArticleViewHolder articleViewHolder, int i){
         articleViewHolder.t.setText(myNews.get(i).getTitle());
         articleViewHolder.im.setText(myNews.get(i).getImgURL());
-        String myDateSection = myNews.get(i).getDate() + " | " + myNews.get(i).getSection();
+        SimpleDateFormat sdfENG = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+        sdfENG.setTimeZone(TimeZone.GMT_ZONE);
+        Date myDateENG = new Date();
+        try {
+            myDateENG = sdfENG.parse(myNews.get(i).getDate());
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+        CharSequence myStringDate = DateUtils.getRelativeTimeSpanString(myDateENG.getTime());
+        String myDateSection = myStringDate.toString() + " | " + myNews.get(i).getSection();
         articleViewHolder.ds.setText(myDateSection);
     }
 }
