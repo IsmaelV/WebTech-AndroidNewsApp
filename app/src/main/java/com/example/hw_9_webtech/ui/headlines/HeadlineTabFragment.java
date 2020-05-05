c863cf4378c55af7837a366d27edab53ef4f160d
IsmaelV <iv2181@columbia.edu>
2020-05-04 19:08:47 -0700

    Added API call and present for all tabs with respective titles | Formatting is still off - it overflows over the tabs

--- app/src/main/java/com/example/hw_9_webtech/ui/headlines/HeadlineTabFragment.java
+++ app/src/main/java/com/example/hw_9_webtech/ui/headlines/HeadlineTabFragment.java
@@ -4,16 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.hw_9_webtech.NewsArticle;
 import com.example.hw_9_webtech.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HeadlineTabFragment extends Fragment {
 
     private String urlCall;
     private String section;
+    private RequestQueue mqueue;
+    private ArrayAdapter myArrayAdapter;
+    private JSONArray results;
+    private List<NewsArticle> all_news;
+    private List<String> tmp_list;  // Only temporarily used to display text in a list
 
     static Fragment getInstance(int position, String s){
         Bundle bundle = new Bundle();
@@ -31,38 +53,57 @@ public class HeadlineTabFragment extends Fragment {
         super.onCreate(savedInstanceState);
         section = getArguments().getString("sectionToShow");
         urlCall = getArguments().getString("urlToShow");
+        mqueue = Volley.newRequestQueue(Objects.requireNonNull(getActivity()));
+        tmp_list = new ArrayList<>();
+        all_news = new ArrayList<>();
+        jsonParse();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View root = inflater.inflate(R.layout.fragment_tab, container, false);
 
-        TextView textView = root.findViewById(R.id.tab_text_view);
-
-        String text_to_set = section + "\n\n" + urlCall;
-        textView.setText(text_to_set);
+        ListView listView = root.findViewById(R.id.headline_list);
+        myArrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()),
+                android.R.layout.simple_list_item_1,
+                tmp_list);
+        listView.setAdapter(myArrayAdapter);
 
         return root;
     }
 
-    private static String getSection(int pos){
-        String result = "error";
+    private void jsonParse() {
+        tmp_list.clear();
+        all_news.clear();
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                urlCall,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            results = response.getJSONObject("response")
+                                    .getJSONArray("results");
 
-        if (pos == 0){
-            result = "world";
-        }
-        else if(pos == 1){
-            result = "politics";
-        }
-        else if(pos == 2){
-            result = "business";
-        }
-        else if(pos == 3){
-            result = "technology";
-        }
-        else if(pos == 4){
-            result = "sports";
-        }
-        return result;
+                            for(int i = 0; i < results.length(); i++){
+                                NewsArticle singleArticle = new NewsArticle(results.getJSONObject(i), "home");
+                                tmp_list.add(singleArticle.getTitle() + ", " + singleArticle.getSection() + ", " + singleArticle.getDate());
+                                all_news.add(singleArticle);
+                            }
+                            myArrayAdapter.notifyDataSetChanged();
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+        mqueue.add(request);
     }
 }
