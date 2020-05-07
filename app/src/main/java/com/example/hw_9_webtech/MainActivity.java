614f98c28a74b1ff7cccc3b08776fe5c52bd5700
IsmaelV <iv2181@columbia.edu>
2020-05-07 03:54:34 -0700

    Added autocomplete functionality | Still not complete

--- app/src/main/java/com/example/hw_9_webtech/MainActivity.java
+++ app/src/main/java/com/example/hw_9_webtech/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.hw_9_webtech;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.SearchView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.MenuItemCompat;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
@@ -27,12 +42,17 @@ import pub.devrel.easypermissions.EasyPermissions;
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_LOCATION_PERMISSION = 1;
+    private List<String> autoSuggestionList;
+    private RequestQueue myQueue;
+    private ArrayAdapter<String> autoCompleteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestLocationPermission();
+//        autoSuggestionList = new ArrayList<>();
+        myQueue = Volley.newRequestQueue(this);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -49,6 +69,18 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.top_main_menu, menu);
         MenuItem searchItem = menu.findItem(R.id.app_bar_search);
         SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        final SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete) searchView.findViewById(R.id.search_src_text);
+        autoSuggestionList = new ArrayList<>();
+        autoCompleteAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, autoSuggestionList);
+        searchAutoComplete.setAdapter(autoCompleteAdapter);
+
+        searchAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String queryString = "" + (String) parent.getItemAtPosition(position);
+                searchAutoComplete.setText(queryString);
+            }
+        });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -63,10 +95,66 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                if(newText.length() <= 2){
+                    autoSuggestionList.clear();
+                    return false;
+                }
+                else{
+                    jsonParse(newText);
+                    return true;
+                }
             }
         });
         return true;
+    }
+
+    private void jsonParse(String query) {
+        String encodedQuery = "";
+        try {
+            encodedQuery = URLEncoder.encode(query, "utf-8");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        String apiURL = "https://api.cognitive.microsoft.com/bing/v7.0/suggestions?q=" + encodedQuery;
+        autoSuggestionList.clear();
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                apiURL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray results = response.getJSONArray("suggestionGroups")
+                                    .getJSONObject(0)
+                                    .getJSONArray("searchSuggestions");
+
+                            for(int i = 0; i < results.length(); i++){
+
+                                autoSuggestionList.add(results.getJSONObject(i).getString("displayText"));
+                            }
+                            autoCompleteAdapter.notifyDataSetChanged();
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }){
+            @Override
+            public Map<String, String> getHeaders(){
+                Map<String, String> params = new HashMap<>();
+                params.put("Ocp-Apim-Subscription-Key", "68b8a16bc26c4d2ba2e8e1f7c08730e0");
+                return params;
+            }
+        };
+        myQueue.add(request);
     }
 
     @Override
