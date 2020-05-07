9c9988f0954d649b2b3d4aacb14c8f2d5eddd957
IsmaelV <iv2181@columbia.edu>
2020-05-06 17:13:21 -0700

    Fixed color themes

--- app/src/main/java/com/example/hw_9_webtech/DetailedArticleActivity.java
+++ app/src/main/java/com/example/hw_9_webtech/DetailedArticleActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailedArticleActivity extends AppCompatActivity {
@@ -12,10 +14,21 @@ public class DetailedArticleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_article);
+        invalidateOptionsMenu();  // Works alongside onCreateOptionsMenu to make custom menu
 
         Intent intent = getIntent();
-        String articleID = intent.getExtras().getString("articleID");
+        String articleID = intent.getExtras().getString("articleID", null);
 
         ((TextView) findViewById(R.id.articleID)).setText(articleID);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.top_detailed_article_menu, menu);
+        return true;
+    }
+
+    public void finishArticle(MenuItem item) {
+        this.finish();
     }
 }
